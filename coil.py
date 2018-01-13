1e5270a2d12cd4db461700e51fbf4df90c5c1d58
tewujianke <liguanduo@gmail.com>
2018-01-12 23:35:22 -0800

    initial uzi

--- coil.py
+++ coil.py
@@ -5,8 +5,8 @@ A python script to enhance PUBG
 
 from __future__ import print_function
 
-#import win32api
-#import win32con
+import win32api
+import win32con
 import time
 import os
 import sys
@@ -26,7 +26,7 @@ class Gun(object):
     def __init__(self,gun_id,recoil_seq,rounds_per_min):
         scalar = 0.2
         self.gun_id = gun_id
-        self.recoil_seq = [x*scalar for x in recoil_seq]
+        self.recoil_seq = [int(x*scalar) for x in recoil_seq]
         self.rounds_per_min = rounds_per_min
 
         self.time_between_shots = 1.0/((rounds_per_min+2)/60)
@@ -51,4 +51,23 @@ if __name__ == '__main__':
 
     a = Uzi()
     print(a.get_recoil())
+    current_gun = Uzi()
+    count = 0
+    while 1:
+        a = win32api.GetAsyncKeyState(win32con.VK_LBUTTON)
+        while a != 0:
+            time.sleep(current_gun.time_between_shots)
+            (curx,cury) = win32api.GetCursorPos()
+            y = cury+current_gun.get_recoil()[count]
+            win32api.SetCursorPos((curx,y))
+            count+=1
+            if count == len(current_gun.get_recoil()):
+                count = 0
+            a = win32api.GetAsyncKeyState(win32con.VK_LBUTTON)
+            if not a:
+                count = 0
+            time.sleep(1.0/((1000.0*2+2))/60)
+        
 
+
+        
